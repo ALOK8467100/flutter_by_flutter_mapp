a5f006f21bb72dbd8580c445b7566376ff700742
Alok kumar <akkumarcse08@gmail.com.com>
2023-06-28 21:30:23 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -17,8 +17,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Hello App "),
         ),
-        body: Text("new app"), // to create a body inside app bar
+        body: Text("new app"),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+                label: "Settings", icon: Icon(Icons.settings)),
+            BottomNavigationBarItem(
+                label: "alarm", icon: Icon(Icons.access_alarm)),
+          ],
+        ),
       ),
     );
   }
 }
+/* In BottomNavigationBar we have to assign at least two items or more than two 
+    in side items we use BottomNavigationBarItem(),
+    inside BottomNavigationBarItem() we use label and icon
+    in label we write the name of that function as text
+
+    in icon we use icon: Icon(Icon.required field)
+    in required field place we put various logo which work according to its functionality
+*/
